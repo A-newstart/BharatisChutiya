45348227512c62513a814b2732067591d8f7c9fd
forbharath41@gmail.com <forbharath41@gmail.com>
2023-09-10 18:05:22 +0530

    .

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -230,7 +230,7 @@ def get_readable_message():
     msg += "╭──────────────────────╮"    
     msg += "\n│        JOIN NOW : <a href='https://t.me/X_leechers'>𝗫 leechers</a>             │"
     msg += "\n╰──────────────────────╯"
-    msg += f"\n <b>DISK</b>: <code>{get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)}</code>"
+    msg += f"\n <b>DISK</b>: <code>{get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}</code>"
     msg += f" | <b>UPTM</b>: <code>{get_readable_time(time() - botStartTime)}</code>"
     msg += f"\n <b>DL</b>: <code>{get_readable_file_size(dl_speed)}/s</code>"
     msg += f"  |  <b>UL</b>: <code>{get_readable_file_size(up_speed)}/s</code>"
