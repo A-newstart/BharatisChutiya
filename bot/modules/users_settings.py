c79091a2badaa90bf92679f6270bde232ee695da
forbharath41@gmail.com <forbharath41@gmail.com>
2023-09-11 07:23:44 +0530

    .

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -197,7 +197,7 @@ async def update_user_settings(query, key=None, edit_type=None, edit_mode=None, 
 @new_thread
 async def user_settings(client, message):
     msg, button = await get_user_settings(message.from_user)
-    user_id = query.from_user.id
+    #user_id = query.from_user.id
     #thumbpath = f"Thumbnails/{user_id}.jpg"
    # if not ospath.exists(thumbpath):
        # thumbpath = 'IMAGES'
