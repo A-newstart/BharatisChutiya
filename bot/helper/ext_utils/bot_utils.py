e1224fffeb1006449c0bc69438866cf484f3b5c8
forbharath41@gmail.com <forbharath41@gmail.com>
2023-09-13 08:29:23 +0530

    .

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -177,7 +177,7 @@ def get_readable_message():
             msg += f"\n❂ User: {source(download)}"
             msg += f"\n❂ Speed: {download.speed()}"
             msg += f"\n❂ Estimated: {download.eta()}"'''
-            msg += f"\n❂ <code>Done    </code>»{download.processed_bytes()}/{download.size()}"
+            msg += f"\n❂ <code>Done    </code>»{download.processed_bytes()} {download.size()}"
             msg += f"\n❂ <code>Speed   </code>»{download.speed()}"
             msg += f"\n❂ <code>User    </code>»{source(download)}"
             msg += f"\n❂ <code>ETA     </code>»{download.eta()}"
@@ -231,9 +231,9 @@ def get_readable_message():
         buttons.ibutton("⌦", "status nex")
         button = buttons.build_menu(3)
 
-    msg += "\n\n\n──────────────────────"    
-    msg += "\n   JOIN NOW : <a href='https://t.me/POWERLEECH'> POWER LEECH</a>        "
-    msg += "\n──────────────────────"
+   # msg += "\n\n\n──────────────────────"    
+    msg += "\n\n   <u>JOIN NOW : <a href='https://t.me/POWERLEECH'> POWER LEECH</a></u>        "
+   # msg += "\n──────────────────────"
     msg += f"\n <b>DISK</b>: <code>{get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}</code>"
     msg += f" | <b>UPTM</b>: <code>{get_readable_time(time() - botStartTime)}</code>"
     msg += f"\n <b>DL</b>: <code>{get_readable_file_size(dl_speed)}/s</code>"
